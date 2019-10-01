cf0f25e108da7a15e48b36cab0eb2855b8d771b1
abiisnn <abigail.nic.say@hotmail.com>
2019-10-01 14:11:47 -0500

    Gusano

---
+++ AbiiSnn/Gusano/Gusano.h
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Gusano {
+public:
+	enum tipoMov {
+		lineaVertical, // 0
+		lineaHorizontal, // 1
+		circulo,
+		medioCirculo
+	};
+	Gusano(int longitud, tipoMov a);
+	int x; 
+	int y;
+private:
+	l;
+};
+
+Gusano::Gusano(int longitud, tipoMov a) {
+
+}
+
+Gusano::moverVertical() {
+	y++;
+}
---
+++ AbiiSnn/Gusano/Movimiento.h
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Movimiento {
+public:
+	Movimiento();
+	int x, y;
+	void linealHorizontal();
+private:
+	
+};
+void Movimiento::linealHorizontal() {
+	
+} 
