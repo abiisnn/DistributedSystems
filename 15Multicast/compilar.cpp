866c644e4037ae4c8dd63250a35560e088f1fa76
abiisnn <abigail.nic.say@hotmail.com>
2019-10-29 20:43:42 -0600

    15 update

--- 15Multicast/compilar.cpp
+++ 15Multicast/compilar.cpp
@@ -1,1 +1,3 @@
 g++ SocketMulticast.cpp PaqueteDatagrama.cpp emisor.cpp -o emisor -std=c++11
+
+g++ SocketMulticast.cpp SocketDatagrama.cpp PaqueteDatagrama.cpp emisor2.cpp -o emisor2 -std=c++11
---
+++ 15Multicast/emisor2.cpp
@@ -0,0 +1,31 @@
+#include "SocketMulticast.h"
+#include "SocketDatagram.h"
+#include "PaqueteDatagrama.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string ipMulticast, cadena;
+	int puerto, ttl;
+	//cin >> ipMulticast >> puerto >> ttl >> cadena;
+	int datos[2];
+	datos[0] = 4;
+	datos[1] = 5;
+	puerto = 7200; ttl = 1;
+	SocketMulticast s(puerto);
+	PaqueteDatagrama a((char *)datos, 2 * sizeof(int), "224.0.0.1", puerto);
+	cout << "HOLA" << endl;
+	while(s.envia(a, ttl) > 0) {
+		cout << "Enviando datos ..." << endl;
+		
+		char result[sizeof(int)];
+		int num[1];
+		PaqueteDatagrama d(sizeof(int));
+		SocketDatagrama c(puerto);
+
+		int n = c.recibe(d);
+		cout <<"Tamaño de recibido: "<< n << endl;
+		memcpy(num, d.obtieneDatos(), sizeof(int));
+		printf("La suma es:: %d \n",num[0]);
+	}
+}
