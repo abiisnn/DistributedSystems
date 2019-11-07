6ca0cd65d072d2dff028194594ab406f06ec376f
BetoDl <humbertodl2709@gmail.com>
2019-11-07 18:03:36 +0000

    multicast confiable no sabemos si funciona

---
+++ 16MulticastConfiable/mensaje.h
@@ -0,0 +1,7 @@
+#define TAM_MAX_DATA 4000//Definicion de identificadores para operaciones permitidas
+#define suma 1// Definicion del formato de mensaje
+struct mensaje{
+    int messageType; //0= Solicitud, 1 = Respuesta
+    unsigned int requestId;   //Identificador del mensaje
+    char arguments[TAM_MAX_DATA]; 
+};
