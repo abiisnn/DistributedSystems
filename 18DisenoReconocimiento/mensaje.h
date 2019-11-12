1df3597dec6df8e7d27c367f0e94acf5ba669a1e
abiisnn <abigail.nic.say@hotmail.com>
2019-11-12 09:15:38 -0600

    otro

--- 18DisenoReconocimiento/mensaje.h
+++ 18DisenoReconocimiento/mensaje.h
@@ -7,14 +7,3 @@ struct mensaje{
     char arguments[TAM_MAX_DATA]; 
 };
 #ifndef Mensaje_H_
-#define Mensaje_H_
-#define TAM_MAX_DATA 4000
-#define suma 1
-
-struct mensaje {
-	int messageType; // Solicitud: 0, Respuesta = 1
-	unsigned int requestId; // Id mensaje
-	int operationId;
-	char arguments[TAM_MAX_DATA];
-};
-#endif
